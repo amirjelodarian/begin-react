dfca5c32099c65cbc0d7552d647258b7c22abe80
amirjelodarian <amirjelodarian@gmail.com>
2022-06-23 19:25:15 +0430

    change git ignore

---
+++ build/precache-manifest.e87036dd9f8dbbaa93773ab0d43d8699.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e709a6bcb0707c64ef7b",
+    "url": "/static/js/main.e709a6bc.chunk.js"
+  },
+  {
+    "revision": "21bc42596119bd3447a9",
+    "url": "/static/js/1.21bc4259.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "226c0390d8838b04b3bf981e412d24df",
+    "url": "/index.html"
+  }
+];
