6764f4fd61683e7b2dee1147d8cd383616b4697b
amirjelodarian <amirjelodarian@gmail.com>
2022-06-23 21:40:50 +0430

    first commit

--- src/components/ResourceList.jsx
+++ src/components/ResourceList.jsx
@@ -2,25 +2,14 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const ResourceList = ({ resource }) =>{
-    const [resources, setResources] = useState([]);
-    const fetchResource  = async (resource) =>{
-        const {data} = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-        setResources(data);
-    }
-    useEffect(() => {
-      fetchResource(resource);
-    }, [resource]);
-    // async componentDidUpdate(prevProps, prevState){
-    //     if(prevProps.response !== this.props.resource){
-    //         const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-    //         this.setState({ resources: response.data });
-    //     }
-    // }
-    
-        return(
-          <div>{resources.length}</div>  
-        );
-    
-}
+  const resources = useResources(resource);
+    return(
+      <ul>
+        {resources.map((record) => 
+            (<li key={record.id}>{record.title}</li>)
+        )}
+      </ul>
+    );
+};
 
 export default ResourceList;
